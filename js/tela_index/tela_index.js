43c86d6d9feb5d58b239810dfbcf70e432ddb2db
cauuabispoo <53126609+cauuabispoo@users.noreply.github.com>
2024-11-01 15:59:24 -0300

    Update tela_index.js

--- js/tela_index/tela_index.js
+++ js/tela_index/tela_index.js
@@ -1,4 +1,4 @@
 document.getElementById("submitButton").addEventListener("click", function () {
     window.location.href = "telas/equipamento.html";
-    localStorage.clear;
+    localStorage.clear();
 });
