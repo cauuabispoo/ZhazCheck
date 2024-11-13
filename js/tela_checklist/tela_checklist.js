f8199b4911a889b85ce6325c6c16c6f546f93e7c
cauuabispoo <53126609+cauuabispoo@users.noreply.github.com>
2024-11-13 08:51:54 -0300

    Update tela_checklist.js

--- js/tela_checklist/tela_checklist.js
+++ js/tela_checklist/tela_checklist.js
@@ -707,7 +707,7 @@ const dados = [
 
 const filtro = {
     'TOUCH': ['touch'],
-    'ACESSÓRIOS': ['capa ', 'hidrogel', 'pelicula ', 'capinha', 'manopla', 'gatilho', 'pistola'],
+    'ACESSÓRIOS': ['capa ', 'hidrogel', 'pelicula ', 'capinha', 'manopla', 'gatilho', 'pistola', 'SUPORTE'],
     'CABO DE COMUNICAÇÃO': ['cabo'],
     'FONTE DE ALIMENTAÇÃO': ['FONTE'],
     'PLACA PRINCIPAL': ['PLACA PRINCIPAL', 'PLACA'],
