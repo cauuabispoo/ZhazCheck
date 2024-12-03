41a6e6fbf744699f98630e04fb0efcb810079584
cauuabispoo <53126609+cauuabispoo@users.noreply.github.com>
2024-12-03 09:28:49 -0300

    Update tela_laudoPronto.js

--- js/tela_laudoPronto/tela_laudoPronto.js
+++ js/tela_laudoPronto/tela_laudoPronto.js
@@ -261,8 +261,9 @@ async function gerarLaudo() {
     if (obs.causaDefeitoSelecionadoGlobal === "mau"){
       mauuso = 'MAUUSO';
     }
+    var peca = "";
     if(obs.pecaSelecionadoGlobal != ""){
-      const peca = filtrarPalavraChave(pecas[obs.pecaSelecionadoGlobal], palavrasChave) || "PEÇA DESCONHECIDA";
+      peca = filtrarPalavraChave(pecas[obs.pecaSelecionadoGlobal], palavrasChave) || "PEÇA DESCONHECIDA";
     }
     var verificaCarcaca;
     var nivel;
