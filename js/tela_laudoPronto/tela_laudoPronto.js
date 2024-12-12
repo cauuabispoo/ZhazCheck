e5d87b3c7cfe4ea61a3cee1a64f7d12dd7ff36b7
cauuabispoo <53126609+cauuabispoo@users.noreply.github.com>
2024-12-12 09:16:10 -0300

    Update tela_laudoPronto.js

--- js/tela_laudoPronto/tela_laudoPronto.js
+++ js/tela_laudoPronto/tela_laudoPronto.js
@@ -153,7 +153,7 @@ async function carregarEquipamentos() {
 
 
 // Lista de palavras que devem ser ignoradas pelo corretor
-const excecoes = ["POWERBOARD", "SYNC"];
+const excecoes = ["POWERBOARD", "SYNC", "WI-FI"];
 
 // Função para corrigir texto com exceções
 async function corrigirTextoComExcecoes(texto) {
